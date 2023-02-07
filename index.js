f79d3768470b651eec7783cc580e02350252abb6
chetandabli <hrku002@gmail.com>
2023-02-08 02:33:58 +0530

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("resume-button-2").onclick = ()=>{
+    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+}
+document.getElementById("resume-button-1").onclick = ()=>{
+    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+}
