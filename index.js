37c7c3360a3052b8af1edda18e2df8b8807aaecd
chetandabli <hrku002@gmail.com>
2023-02-08 02:52:06 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.getElementById("resume-button-2").onclick = ()=>{
+document.getElementById("resume-link-2").onclick = ()=>{
     window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
 }
-document.getElementById("resume-button-1").onclick = ()=>{
+document.getElementById("resume-link-1").onclick = ()=>{
     window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
 }
