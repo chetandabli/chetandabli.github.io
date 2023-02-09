b030119e58a659abfbcfe5aaf712c5d0f6944f3a
chetandabli <hrku002@gmail.com>
2023-02-10 01:32:05 +0530

    added file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("resume-link-2").onclick = ()=>{
-    window.location.assign("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
 }
 document.getElementById("resume-link-1").onclick = ()=>{
-    window.location.assign("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
+    window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
 }
