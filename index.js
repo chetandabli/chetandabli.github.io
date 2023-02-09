302339ba3e3965a3d9978bb4c4bea10ea69fae1a
chetandabli <hrku002@gmail.com>
2023-02-10 02:31:37 +0530

    project link updated

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ document.getElementById("resume-link-2").onclick = ()=>{
 document.getElementById("resume-link-1").onclick = ()=>{
     window.open("https://drive.google.com/file/d/188lGyqT4ZZ3LTYQ9EIKTVzoZKuqeabjy/view")
 }
+document.getElementById("upwear").onclick = ()=>{
+    window.open("https://extraordinary-pavlova-536bcb.netlify.app/")
+}
+document.getElementById("groceryarc").onclick = ()=>{
+    window.open("https://lucent-valkyrie-3c86fc.netlify.app/")
+}
+document.getElementById("anthropologie").onclick = ()=>{
+    window.open("https://spontaneous-dolphin-60e6a7.netlify.app")
+}
+document.getElementById("kindmeal").onclick = ()=>{
+    window.open("https://beamish-salamander-14c16f.netlify.app/")
+}
